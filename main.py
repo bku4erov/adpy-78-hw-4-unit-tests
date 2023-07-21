6ea46c9d71bd09eceecf80af718e6749673b704f
Boris Kucherov <boris.ku4erov@gmail.com>
2023-07-21 19:44:22 +0300

    edit prints to functions

--- main.py
+++ main.py
@@ -1,63 +1,87 @@
-# Задание 1
-geo_logs = [
-    {'visit1': ['Москва', 'Россия']},
-    {'visit2': ['Дели', 'Индия']},
-    {'visit3': ['Владимир', 'Россия']},
-    {'visit4': ['Лиссабон', 'Португалия']},
-    {'visit5': ['Париж', 'Франция']},
-    {'visit6': ['Лиссабон', 'Португалия']},
-    {'visit7': ['Тула', 'Россия']},
-    {'visit8': ['Тула', 'Россия']},
-    {'visit9': ['Курск', 'Россия']},
-    {'visit10': ['Архангельск', 'Россия']}
-]
+# # Задание 1
+# geo_logs = [
+#     {'visit1': ['Москва', 'Россия']},
+#     {'visit2': ['Дели', 'Индия']},
+#     {'visit3': ['Владимир', 'Россия']},
+#     {'visit4': ['Лиссабон', 'Португалия']},
+#     {'visit5': ['Париж', 'Франция']},
+#     {'visit6': ['Лиссабон', 'Португалия']},
+#     {'visit7': ['Тула', 'Россия']},
+#     {'visit8': ['Тула', 'Россия']},
+#     {'visit9': ['Курск', 'Россия']},
+#     {'visit10': ['Архангельск', 'Россия']}
+# ]
 
-for visit in geo_logs.copy():
-  if 'Россия' not in [country for city, country in list(visit.values())]:
-    geo_logs.remove(visit)
+# for visit in geo_logs.copy():
+#     if 'Россия' not in [country for city, country in list(visit.values())]:
+#         geo_logs.remove(visit)
 
-print(geo_logs)
+# print(geo_logs)
 
 # Задание 2
-ids = {'user1': [213, 213, 213, 15, 213],
-       'user2': [54, 54, 119, 119, 119],
-       'user3': [213, 98, 98, 35]}
+# Выведите на экран все уникальные гео-ID из значений словаря ids.
+# Т.е. список вида [213, 15, 54, 119, 98, 35]
+def get_unique_ids(ids):
+    unique_ids = {user_id for user_ids in list(
+        ids.values()) for user_id in user_ids}
+    result = list(unique_ids)
+    result.sort()
+    return result
 
-unique_ids = {user_id for user_ids in list(ids.values()) for user_id in user_ids}
-print(list(unique_ids))
 
 # Задание 3
-queries = [
-    'смотреть сериалы онлайн',
-    'новости спорта',
-    'афиша кино',
-    'курс доллара',
-    'сериалы этим летом',
-    'курс по питону',
-    'сериалы про спорт'
-    ]
+# Дан список поисковых запросов. Получить распределение количества слов в них.
+# Т.е. поисковых запросов из одного - слова 5%, из двух - 7%, из трех - 3% и т.д.
+def queries_by_words_distr(queries):
+    queries_len = [query.count(' ') + 1 for query in queries]
+    queries_len_distr = {len_val: queries_len.count(
+        len_val) for len_val in set(queries_len)}
+    queries_count = sum(queries_len_distr.values())
+    queries_distr = [(query_len, round(queries_len_distr[query_len] /
+                      queries_count * 100)) for query_len in queries_len_distr]
+    queries_distr.sort(key=lambda x: x[0])
+    return queries_distr
 
-queries_len = [query.count(' ') + 1 for query in queries]
-queries_len_distr = {len_val: queries_len.count(len_val) for len_val in set(queries_len)}
-queries_count = sum(queries_len_distr.values())
-print(*[f'Поисковых запросов из {query_len} слов(а) - {round(queries_len_distr[query_len] / queries_count * 100)}%' for query_len in queries_len_distr], sep='\n')
 
-# Задание 4
-stats = {'facebook': 55, 'yandex': 120, 'vk': 115, 'google': 99, 'email': 42, 'ok': 98}
+# # Задание 4
+# stats = {'facebook': 55, 'yandex': 120, 'vk': 115,
+#         'google': 99, 'email': 42, 'ok': 98}
 
-max_sales = max(stats.values())
-# Вывод всех каналов, у которых максимальный объем продаж
-print(*[chanel for chanel, sales in stats.items() if sales == max_sales], sep='\n')
+# max_sales = max(stats.values())
+# # Вывод всех каналов, у которых максимальный объем продаж
+# print(*[chanel for chanel, sales in stats.items() if sales == max_sales], sep='\n')
 
 # Задание 5
-input_list = ['2018-01-01', 'yandex', 'cpc', 100]
+# Напишите код для преобразования произвольного списка вида ```['2018-01-01', 'yandex', 'cpc', 100]``` (он может быть любой длины)
+# в словарь ```{'2018-01-01': {'yandex': {'cpc': 100}}}```
+def list_to_dict(input_list):
+    if len(input_list) < 2:
+        raise 'Для создания словаря из списка в нем должно быть минимум 2 элемента'
 
-if len(input_list) < 2:
-  print('Для создания словаря из списка в нем должно быть минимум 2 элемента')
-  exit()
+    dict_from_list = {input_list[-2]: input_list[-1]}
+    for list_item in reversed(input_list[:-2]):
+        dict_from_list = {list_item: dict_from_list}
 
-dict_from_list = {input_list[-2]: input_list[-1]}
-for list_item in reversed(input_list[:-2]):
-  dict_from_list = {list_item: dict_from_list}
+    return dict_from_list
 
-print(dict_from_list)
+
+if __name__ == '__main__':
+    ids = {'user1': [213, 213, 213, 15, 213],
+			'user2': [54, 54, 119, 119, 119],
+			'user3': [213, 98, 98, 35]}
+    print(get_unique_ids(ids))
+
+    queries = [
+        'смотреть сериалы онлайн',
+        'новости спорта',
+        'афиша кино',
+        'курс доллара',
+        'сериалы этим летом',
+        'курс по питону',
+        'сериалы про спорт'
+    ]
+    print(queries_by_words_distr(queries))
+
+    input_list = ['2018-01-01', 'yandex', 'cpc', 100]
+    print(list_to_dict(input_list))
+	
